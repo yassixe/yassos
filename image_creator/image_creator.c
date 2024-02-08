17f9a40fe9de1c5aab973d30512a0014e0b1ed5b
elkhaldiyassin <yassin.elkhaldi.ahannach@hotmail.com>
2024-02-08 12:50:13 -0500

    just tired, ill come back later after reading the uefi spec

--- image_creator/image_creator.c
+++ image_creator/image_creator.c
@@ -1,40 +1,58 @@
 #include "image_creator.h"
 
-char* result_name = "image.bin";
-
-int main(void){
-    FILE *image = fopen(result_name,"wb+");
+char *result_name = "image.bin";
+uint32_t block_size = DEFAULT_BLOCK_SIZE;
+int main(void)
+{
+    FILE *image = fopen(result_name, "wb+");
     bool True = false;
 
-    if(!image){
+    if (!image)
+    {
         printf("error creating image\n");
         return 1;
     }
-    if((True = write_mbr(image)) == false) return 1; 
+    if ((True = write_mbr(image)) == false)
+        return 1;
 
     return 0;
 }
 
-bool write_mbr(FILE* image){
+bool write_mbr(FILE *image)
+{
     partition_record pr_ = {
         .boot_indicator = 0,
-        .starting_chs =  {0x00,0x02,0x00},
-        .os_type =0xee,
-        .ending_chs = {0xff,0xff,0xff},
+        .starting_chs = {0x00, 0x02, 0x00},
+        .os_type = 0xee,
+        .ending_chs = {0xff, 0xff, 0xff},
         .starting_lba = 0x1,
-        .size_in_lba = 0xffffffff
-    };
+        .size_in_lba = 0xffffffff};
     mbr mbr_ = {
         .boot_code = {0},
-        .signature  = 0,
+        .signature = 0,
         .unkown = 0,
-        .partition_table = {pr_,
-                            (partition_record){0},
-                            (partition_record){0},
-                            (partition_record){0},},
-        .end_signature = 0xAA55
-    };
-    size_t w_size = fwrite(&mbr_,1,sizeof(mbr_),image);
-    printf("the written size is : %ld\n",w_size);
+        .partition_table = {
+            pr_,
+            (partition_record){0},
+            (partition_record){0},
+            (partition_record){0},
+        },
+        .end_signature = 0xAA55};
+    size_t w_size = fwrite(&mbr_, 1, sizeof(mbr_), image);
+    printf("the written size is : %ld\n", w_size);
     return true;
 }
+
+// bool write_gpt_primary_header(FILE* image){
+//     gpt_header gpth_ = {
+//         .signature = 0x5452415020494645, //8byte encoded of “EFI PART”
+//         .revision =  0x00010000, // revision 1.0
+//         .header_size = 92, // spec say it must >= 92 and < Logical block size
+//         .header_crc32 = 0xB0DFF252, // crc 32 of 92
+//         .reserved = 0,
+//         .my_lba = 1,
+//         .alternate_lba = 0, // the last lba for the backup header
+//         .first_usable_lba = 2,
+//         .last_usable_lba = 0, //last usabale lba
+//     }
+// }
--- image_creator/image_creator.h
+++ image_creator/image_creator.h
@@ -4,26 +4,44 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <assert.h>
-
-typedef struct partition_record{
+#define DEFAULT_BLOCK_SIZE 512
+typedef struct partition_record
+{
     uint8_t boot_indicator;
     uint8_t starting_chs[3];
     uint8_t os_type;
     uint8_t ending_chs[3];
     uint32_t starting_lba;
     uint32_t size_in_lba;
-}__attribute__((packed)) partition_record;
+} __attribute__((packed)) partition_record;
 
-typedef struct mbr{
-    uint8_t             boot_code[440];
-    uint32_t            signature;
-    uint16_t            unkown;
-    partition_record    partition_table[4];
-    uint16_t            end_signature;
-}__attribute__((packed)) mbr;
+typedef struct mbr
+{
+    uint8_t boot_code[440];
+    uint32_t signature;
+    uint16_t unkown;
+    partition_record partition_table[4];
+    uint16_t end_signature;
+} __attribute__((packed)) mbr;
 
+typedef struct gpt_header
+{
+    uint64_t signature;
+    uint32_t revision;
+    uint32_t header_size;
+    uint32_t header_crc32;
+    uint32_t reserved;
+    uint64_t my_lba;
+    uint64_t alternate_lba;
+    uint64_t first_usable_lba;
+    uint64_t last_usable_lba;
+    uint64_t disk_guid[2];
+    uint64_t partition_entry_lba;
+    uint32_t number_of_partition_entries;
+    uint32_t size_of_partition_entries;
+    uint32_t partition_entry_crc32;
+} __attribute__((packed)) gpt_header;
 
-
-bool write_mbr(FILE* image);
+bool write_mbr(FILE *image);
 
 #endif
